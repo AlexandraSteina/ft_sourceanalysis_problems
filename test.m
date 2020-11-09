49b70ff3bf9e521172e121a6b1307a3da7abee4f
AlexandraSteina <74171284+AlexandraSteina@users.noreply.github.com>
2020-11-09 17:56:43 +0100

    Update test.m

--- test.m
+++ test.m
@@ -1,6 +1,6 @@
    %load('hdm_grid_ft_november.mat') % there's a difference in the grid.pos field depending on the Fieldtrip version. 
    load('hdm_grid_ft_april.mat')
-   load('freq.mat')
+   load('freq.mat') % the link for the freq.mat file is found in the Readme
    % 
    % yourfieldtrippath = Try out different fieldtrip releases (fieldtrip-20190922, fieldtrip-20200409, fieldtrip-20200607, fieldtrip-20201103) using hdm_grid_ft_april.mat. 
    % Then use hdm_grid_ft_november.mat together with fieldtrip-20201103 and hdm_grid_ft_april.mat with fieldtrip version fieldtrip-20200409
